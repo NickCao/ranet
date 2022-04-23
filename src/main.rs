3497fd1231809cc92009dfe6a1dc40d74fe0cdf4
Nick Cao <nickcao@nichi.co>
2022-04-23 20:40:46 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,93 @@
-use futures::stream::TryStreamExt;
-use futures::StreamExt;
+use futures::{stream::TryStreamExt, StreamExt};
 use genetlink::{GenetlinkError, GenetlinkHandle};
-use netlink_packet_core::{NetlinkMessage, NetlinkPayload, NLM_F_DUMP, NLM_F_REQUEST};
+use netlink_packet_core::{NetlinkMessage, NLM_F_REQUEST};
 use netlink_packet_generic::GenlMessage;
 use netlink_packet_wireguard::{
-    nlas::{WgAllowedIpAttrs, WgDeviceAttrs, WgPeer, WgPeerAttrs},
+    nlas::{WgDeviceAttrs, WgPeer, WgPeerAttrs},
     Wireguard, WireguardCmd,
 };
 use rand::Rng;
 use rtnetlink::{
     packet::rtnl::constants,
-    packet::rtnl::link::nlas::{Info, InfoKind, Nla},
+    packet::{
+        rtnl::link::nlas::{Info, InfoKind, Nla},
+        LinkMessage,
+    },
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let handle = _create_wg().await;
-    assert!(handle.is_ok());
-    let handle = _print_wg().await;
-    assert!(handle.is_ok());
+    ensure_link("wg0").await?;
     Ok(())
 }
 
-const IFACE_NAME: &str = "wg0";
-
-async fn _create_wg() -> Result<rtnetlink::LinkHandle, rtnetlink::Error> {
-    let (conn, handle, _) = rtnetlink::new_connection().unwrap();
-    tokio::spawn(conn);
-    let mut link_handle = handle.link();
-    let mut req = link_handle.add();
-    let mutator = req.message_mut();
-    let info = Nla::Info(vec![Info::Kind(InfoKind::Wireguard)]);
-    mutator.header.flags = constants::IFF_UP;
-    mutator.nlas.push(info);
-    mutator.nlas.push(Nla::Master(136));
-    mutator.nlas.push(Nla::Group(1));
-    mutator.nlas.push(Nla::Mtu(1400));
-    mutator.nlas.push(Nla::IfName(IFACE_NAME.to_owned()));
-    req.execute().await?;
-    let mut query = link_handle
-        .get()
-        .match_name(IFACE_NAME.to_owned())
-        .execute();
-    while let Ok(Some(link)) = query.try_next().await {
-        let ip: ipnetwork::IpNetwork = "fe80::1/64".parse().unwrap();
-        handle
-            .address()
-            .add(link.header.index, ip.ip(), ip.prefix())
-            .execute()
-            .await?
-    }
-
-    Ok(link_handle)
+enum LinkRequest {
+    Add(rtnetlink::LinkAddRequest),
+    Set(rtnetlink::LinkSetRequest),
 }
 
+impl LinkRequest {
+    fn message(&mut self) -> &mut rtnetlink::packet::LinkMessage {
+        match self {
+            Self::Add(r) => r.message_mut(),
+            Self::Set(r) => r.message_mut(),
+        }
+    }
+    async fn execute(self) -> Result<(), rtnetlink::Error> {
+        match self {
+            Self::Add(r) => r.execute().await,
+            Self::Set(r) => r.execute().await,
+        }
+    }
+}
+
+async fn link_id_by_name(handle: &rtnetlink::Handle, name: &str) -> Option<u32> {
+    let mut links = handle.link().get().match_name(name.to_string()).execute();
+    // FIXME: check error
+    if let Ok(Some(link)) = links.try_next().await {
+        Some(link.header.index)
+    } else {
+        None
+    }
+}
+
+async fn ensure_link(name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let (rtc, rt, _) = rtnetlink::new_connection().unwrap();
+    tokio::spawn(rtc);
+    let mut req = if let Some(id) = link_id_by_name(&rt, name).await {
+        LinkRequest::Set(rt.link().set(id))
+    } else {
+        LinkRequest::Add(rt.link().add())
+    };
+    if let LinkRequest::Add(_) = req {
+        req.message().nlas.push(Nla::IfName(name.to_string()));
+    }
+    let msg = req.message();
+    msg.header.flags = constants::IFF_UP;
+    msg.nlas
+        .push(Nla::Info(vec![Info::Kind(InfoKind::Wireguard)]));
+    msg.nlas.push(Nla::Master(136));
+    msg.nlas.push(Nla::Group(1));
+    msg.nlas.push(Nla::Mtu(1400));
+    req.execute().await?;
+    let id = link_id_by_name(&rt, name).await.unwrap();
+    if !rt
+        .address()
+        .get()
+        .set_link_index_filter(id)
+        .execute()
+        .try_collect::<Vec<rtnetlink::packet::rtnl::AddressMessage>>()
+        .await
+        .into_iter()
+        .flatten()
+        .any(|addr| addr.header.scope == rtnetlink::packet::rtnl::constants::RT_SCOPE_LINK)
+    {
+        // ll not found, generate a random one
+    }
+    Ok(())
+}
+
+/*
 async fn _print_wg() -> Result<GenetlinkHandle, GenetlinkError> {
     let (conn, mut handle, _) = genetlink::new_connection().unwrap();
     tokio::spawn(conn);
@@ -76,4 +110,4 @@ async fn _print_wg() -> Result<GenetlinkHandle, GenetlinkError> {
     handle.notify(nlmsg).await?;
     Ok(handle)
 }
-
+*/
