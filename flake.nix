9849aa943d6997d6fbd2e9934a1c2b8b61778d8b
Nick Cao <nickcao@nichi.co>
2022-04-23 23:05:33 +0800

    add initial rait compat

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NickCao/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs;[ cargo rustc rustfmt pkg-config openssl rust-analyzer ];
+          };
+        }
+      );
+}
--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,56 @@ use ranet::wgctrl::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ensure_link(&LinkConfig {
-        name: "wg0".to_string(),
-        group: 1,
-        master: 0,
-        mtu: 1400,
-    })
-    .await?;
-    ensure_wireguard(&WireguardConfig {
-        name: "wg0".to_string(),
-        private_key: rand::thread_rng().gen(),
-        listen_port: 12345,
-        fwmark: 67,
-        peer: PeerConfig {
-            public_key: rand::thread_rng().gen(),
-            endpoint: std::net::SocketAddr::new(
-                std::net::IpAddr::V4(std::net::Ipv4Addr::new(1, 2, 3, 4)),
-                5678,
-            ),
-            keep_alive: 15,
-        },
-    })
-    .await?;
+    let private_key = rand::thread_rng().gen();
+    let send_port = 12345;
+    let group = 10;
+    let master = 0;
+    let mtu = 1400;
+    let fwmark = 67;
+    let peers: Vec<ranet::Peer> =
+        reqwest::get("example.com")
+            .await?
+            .json()
+            .await?;
+    for peer in peers {
+        for endpoint in peer.endpoints {
+            let addr = match tokio::net::lookup_host((endpoint.address, send_port)).await {
+                Ok(mut hosts) => hosts
+                    .find(|addr| match endpoint.address_family.as_str() {
+                        "ip4" => addr.is_ipv4(),
+                        "ip6" => addr.is_ipv6(),
+                        _ => true,
+                    })
+                    .unwrap_or(std::net::SocketAddr::new(
+                        std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)),
+                        0,
+                    )),
+                Err(_) => std::net::SocketAddr::new(
+                    std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)),
+                    0,
+                ),
+            };
+            let name = format!("wg{}", endpoint.send_port);
+            ensure_link(&LinkConfig {
+                name: name.to_string(),
+                group,
+                master,
+                mtu,
+            })
+            .await?;
+            ensure_wireguard(&WireguardConfig {
+                name: name.to_string(),
+                private_key,
+                listen_port: endpoint.send_port,
+                fwmark,
+                peer: PeerConfig {
+                    public_key: peer.public_key,
+                    endpoint: addr,
+                    keep_alive: 20,
+                },
+            })
+            .await?;
+        }
+    }
     Ok(())
 }
