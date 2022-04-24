5c693fd3ea1114d8b3421a81773c4c8404aeb481
Nick Cao <nickcao@nichi.co>
2022-04-24 13:50:37 +0800

    add packages

--- flake.nix
+++ flake.nix
@@ -6,11 +6,18 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs;[ cargo rustc rustfmt rust-analyzer ];
+        let pkgs = import nixpkgs { inherit system; }; in
+        rec {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs;[ rustfmt rust-analyzer ];
+            inputsFrom = [ packages.default ];
+          };
+          packages.default = pkgs.rustPlatform.buildRustPackage {
+            name = "ranet";
+            src = self;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
           };
         }
       );
