85b1cede66960128b3dde61274a03ae0bfda3b69
Nick Cao <nickcao@nichi.co>
2022-04-24 10:54:10 +0800

    remove requwest

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ struct Args {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = argh::from_env();
     let cfg: Config = serde_json::from_slice(&std::fs::read(args.config).unwrap()).unwrap();
-    let peers: Vec<ranet::Peer> = reqwest::get(cfg.registry).await?.json().await?;
+    let peers: Vec<ranet::Peer> =
+        serde_json::from_slice(&std::fs::read(cfg.registry).unwrap()).unwrap();
     for transport in cfg.transport {
         for peer in &peers {
             for endpoint in &peer.endpoints {
