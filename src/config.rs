6db9d9f8c3d7f99063c70659c6380e365481cf27
Nick Cao <nickcao@nichi.co>
2022-04-24 13:44:53 +0800

    add comments and sanity check

--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,43 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 use serde_with::base64::Base64;
 use serde_with::serde_as;
 
 #[serde_as]
 #[derive(Deserialize, Serialize, Debug)]
+/// ranet config
 pub struct Config {
+    /// path to registry file
     pub registry: String,
     #[serde_as(as = "Base64")]
+    /// wireguard private key
     pub private_key: [u8; 32],
+    /// vrf for interfaces
     pub vrf: String,
+    /// wireguard config
     pub transport: Vec<Transport>,
+    /// group for stale interfaces
     pub stale_group: u32,
+    /// group for active interfaces
     pub active_group: u32,
+    /// remarks for humans
+    pub remarks: HashMap<String, String>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
+/// wireguard config
 pub struct Transport {
+    /// address family, ip4 or ip6
     pub address_family: String,
+    /// wireguard send port
     pub send_port: u16,
+    /// interface mtu
     pub mtu: u32,
-    pub ifprefix: String,
+    /// interface name prefix
+    pub prefix: String,
+    /// wireguard fwmark
     pub fwmark: u32,
+    /// listen on random port
     pub random_port: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use argh::FromArgs;
 use ranet::config::*;
 use ranet::link::*;
 use ranet::wgctrl::*;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 #[derive(FromArgs)]
 /// ranet - redundant array of inexpensive tunnels
@@ -35,16 +36,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = argh::from_env();
     let cfg: Config = serde_json::from_slice(&std::fs::read(args.config)?)?;
     let peers: Vec<ranet::Peer> = serde_json::from_slice(&std::fs::read(cfg.registry)?)?;
+    // stale group and active group must be set
     assert_ne!(cfg.stale_group, 0);
     assert_ne!(cfg.active_group, 0);
-
+    // address family must be one of ip4 or ip6
+    assert!(cfg
+        .transport
+        .iter()
+        .all(|t| ["ip4", "ip6"].contains(&t.address_family.as_str())));
     match args.command {
         Command::Up(_) => {
             let (conn, handle, _) = rtnetlink::new_connection()?;
             tokio::spawn(conn);
+            // mark all existing interfaces as stale
             group_change(&handle, cfg.active_group, cfg.stale_group).await?;
+            // get index of vrf device
             let master = index_query(&handle, &cfg.vrf).await?.unwrap();
-            for transport in cfg.transport {
+            for transport in &cfg.transport {
                 for peer in &peers {
                     for endpoint in &peer.endpoints {
                         if transport.address_family != endpoint.address_family {
@@ -55,10 +63,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             transport.send_port,
                         ))
                         .await;
-                        let lo = std::net::SocketAddr::new(
-                            std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)),
-                            0,
-                        );
+                        let dummy = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0);
                         let addr = match hosts {
                             Ok(mut hosts) => hosts
                                 .find(|addr| match endpoint.address_family.as_str() {
@@ -66,10 +71,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                     "ip6" => addr.is_ipv6(),
                                     _ => false,
                                 })
-                                .unwrap_or(lo),
-                            Err(_) => lo,
+                                .unwrap_or(dummy),
+                            Err(_) => dummy,
                         };
-                        let name = format!("{}{}", transport.ifprefix, endpoint.send_port);
+                        let name = format!("{}{}", transport.prefix, endpoint.send_port);
                         ensure(
                             &handle,
                             &LinkConfig {
@@ -83,7 +88,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         ensure_wireguard(&WireguardConfig {
                             name: name.to_string(),
                             private_key: cfg.private_key,
-                            listen_port: endpoint.send_port,
+                            listen_port: if transport.random_port {
+                                0
+                            } else {
+                                endpoint.send_port
+                            },
                             fwmark: transport.fwmark,
                             peer: PeerConfig {
                                 public_key: peer.public_key,
@@ -95,12 +104,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     }
                 }
             }
+            // remove stale interfaces
             group_remove(&handle, cfg.stale_group).await?;
             Ok(())
         }
         Command::Down(_) => {
             let (conn, handle, _) = rtnetlink::new_connection()?;
             tokio::spawn(conn);
+            // remove all interfaces
             group_remove(&handle, cfg.stale_group).await?;
             group_remove(&handle, cfg.active_group).await?;
             Ok(())
