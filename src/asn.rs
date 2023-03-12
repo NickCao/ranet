9f00998276ed496a5ad917db70c7316ce9a34a22
Nick Cao <nickcao@nichi.co>
2023-03-12 23:09:42 +0800

    dont take ownership

--- src/asn.rs
+++ src/asn.rs
@@ -5,22 +5,28 @@ use rasn::{
 use rasn_pkix::{AttributeTypeAndValue, RdnSequence, RelativeDistinguishedName};
 
 pub fn encode_identity(
-    organization: String,
-    common_name: String,
-    serial_number: String,
+    organization: &str,
+    common_name: &str,
+    serial_number: &str,
 ) -> Result<String, Error> {
     let rdn = RdnSequence::from([
         RelativeDistinguishedName::from([AttributeTypeAndValue {
             r#type: Oid::JOINT_ISO_ITU_T_DS_ATTRIBUTE_TYPE_ORGANISATION_NAME.into(),
-            value: Any::new(rasn::der::encode(&PrintableString::new(organization))?),
+            value: Any::new(rasn::der::encode(&PrintableString::new(
+                organization.to_string(),
+            ))?),
         }]),
         RelativeDistinguishedName::from([AttributeTypeAndValue {
             r#type: Oid::JOINT_ISO_ITU_T_DS_ATTRIBUTE_TYPE_COMMON_NAME.into(),
-            value: Any::new(rasn::der::encode(&PrintableString::new(common_name))?),
+            value: Any::new(rasn::der::encode(&PrintableString::new(
+                common_name.to_string(),
+            ))?),
         }]),
         RelativeDistinguishedName::from([AttributeTypeAndValue {
             r#type: Oid::JOINT_ISO_ITU_T_DS_ATTRIBUTE_TYPE_SERIAL_NUMBER.into(),
-            value: Any::new(rasn::der::encode(&PrintableString::new(serial_number))?),
+            value: Any::new(rasn::der::encode(&PrintableString::new(
+                serial_number.to_string(),
+            ))?),
         }]),
     ]);
     Ok(format!("asn1dn:#{}", hex::encode(rasn::der::encode(&rdn)?)))
@@ -30,12 +36,7 @@ pub fn encode_identity(
 mod test {
     #[test]
     fn encode() {
-        let identity = super::encode_identity(
-            "acme organization".to_string(),
-            "some server".to_string(),
-            "0".to_string(),
-        )
-        .unwrap();
+        let identity = super::encode_identity("acme organization", "some server", "0").unwrap();
         assert_eq!(
             identity,
             "asn1dn:#303e311a3018060355040a131161636d65206f7267616e697a6174696f6e311430120603550403130b736f6d6520736572766572310a30080603550405130130",
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use config::Config;
 use registry::Registry;
 
+pub mod asn;
 pub mod config;
+pub mod key;
 pub mod registry;
 pub mod vici;
-pub mod asn;
-pub mod key;
 
 pub fn up(config: &Config, registry: &Registry) -> std::io::Result<()> {
     Ok(())
