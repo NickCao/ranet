091e8166fff025d57e5ccaae31d44123762c8e97
Nick Cao <nickcao@nichi.co>
2023-03-14 18:05:05 +0800

    encode asn1 with openssl

--- src/asn.rs
+++ src/asn.rs
@@ -1,35 +1,16 @@
-use rasn::{
-    ber::enc::Error,
-    types::{Any, Oid, PrintableString},
-};
-use rasn_pkix::{AttributeTypeAndValue, RdnSequence, RelativeDistinguishedName};
+use openssl::error::ErrorStack;
 
 pub fn encode_identity(
     organization: &str,
     common_name: &str,
     serial_number: &str,
-) -> Result<String, Error> {
-    let rdn = RdnSequence::from([
-        RelativeDistinguishedName::from([AttributeTypeAndValue {
-            r#type: Oid::JOINT_ISO_ITU_T_DS_ATTRIBUTE_TYPE_ORGANISATION_NAME.into(),
-            value: Any::new(rasn::der::encode(&PrintableString::new(
-                organization.to_string(),
-            ))?),
-        }]),
-        RelativeDistinguishedName::from([AttributeTypeAndValue {
-            r#type: Oid::JOINT_ISO_ITU_T_DS_ATTRIBUTE_TYPE_COMMON_NAME.into(),
-            value: Any::new(rasn::der::encode(&PrintableString::new(
-                common_name.to_string(),
-            ))?),
-        }]),
-        RelativeDistinguishedName::from([AttributeTypeAndValue {
-            r#type: Oid::JOINT_ISO_ITU_T_DS_ATTRIBUTE_TYPE_SERIAL_NUMBER.into(),
-            value: Any::new(rasn::der::encode(&PrintableString::new(
-                serial_number.to_string(),
-            ))?),
-        }]),
-    ]);
-    Ok(format!("asn1dn:#{}", hex::encode(rasn::der::encode(&rdn)?)))
+) -> Result<String, ErrorStack> {
+    let mut b = openssl::x509::X509NameBuilder::new()?;
+    b.append_entry_by_text("O", organization)?;
+    b.append_entry_by_text("CN", common_name)?;
+    b.append_entry_by_text("serialNumber", serial_number)?;
+    let name = b.build().to_der()?;
+    Ok(format!("asn1dn:#{}", hex::encode(&name)))
 }
 
 #[cfg(test)]
@@ -39,7 +20,7 @@ mod test {
         let identity = super::encode_identity("acme organization", "some server", "0").unwrap();
         assert_eq!(
             identity,
-            "asn1dn:#303e311a3018060355040a131161636d65206f7267616e697a6174696f6e311430120603550403130b736f6d6520736572766572310a30080603550405130130",
+            "asn1dn:#303e311a3018060355040a0c1161636d65206f7267616e697a6174696f6e3114301206035504030c0b736f6d6520736572766572310a30080603550405130130",
         );
     }
 }
