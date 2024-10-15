b3b990bb09bc072320d9e706cd313163a8940925
Nick Cao <nickcao@nichi.co>
2024-10-14 23:04:42 -0400

    Encode asn1 with der

--- src/asn.rs
+++ src/asn.rs
@@ -1,15 +1,31 @@
-use openssl::error::ErrorStack;
+use const_oid::db::rfc4519::{CN, ORGANIZATION_NAME, SERIAL_NUMBER};
+use x509_cert::{
+    attr::AttributeTypeAndValue,
+    der::asn1::{PrintableString, SetOfVec, Utf8StringRef},
+    der::{Encode, Result},
+    name::{RdnSequence, RelativeDistinguishedName},
+};
 
 pub fn encode_identity(
     organization: &str,
     common_name: &str,
     serial_number: &str,
-) -> Result<String, ErrorStack> {
-    let mut b = openssl::x509::X509NameBuilder::new()?;
-    b.append_entry_by_text("O", organization)?;
-    b.append_entry_by_text("CN", common_name)?;
-    b.append_entry_by_text("serialNumber", serial_number)?;
-    let name = b.build().to_der()?;
+) -> Result<String> {
+    let name = RdnSequence(vec![
+        RelativeDistinguishedName(SetOfVec::from_iter([AttributeTypeAndValue {
+            oid: ORGANIZATION_NAME,
+            value: Utf8StringRef::new(organization)?.into(),
+        }])?),
+        RelativeDistinguishedName(SetOfVec::from_iter([AttributeTypeAndValue {
+            oid: CN,
+            value: Utf8StringRef::new(common_name)?.into(),
+        }])?),
+        RelativeDistinguishedName(SetOfVec::from_iter([AttributeTypeAndValue {
+            oid: SERIAL_NUMBER,
+            value: (&PrintableString::new(serial_number)?).into(),
+        }])?),
+    ])
+    .to_der()?;
     Ok(format!("asn1dn:#{}", hex::encode(name)))
 }
 
