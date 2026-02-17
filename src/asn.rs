8637a7ed0796c23de12489fcff60313cace0d784
Nick Cao <nickcao@nichi.co>
2026-02-16 20:56:08 -0500

    bump deps

--- src/asn.rs
+++ src/asn.rs
@@ -11,16 +11,16 @@ pub fn encode_identity(
     common_name: &str,
     serial_number: &str,
 ) -> Result<String> {
-    let name = RdnSequence(vec![
-        RelativeDistinguishedName(SetOfVec::from_iter([AttributeTypeAndValue {
+    let name = RdnSequence::from(vec![
+        RelativeDistinguishedName::from(SetOfVec::from_iter([AttributeTypeAndValue {
             oid: ORGANIZATION_NAME,
             value: Utf8StringRef::new(organization)?.into(),
         }])?),
-        RelativeDistinguishedName(SetOfVec::from_iter([AttributeTypeAndValue {
+        RelativeDistinguishedName::from(SetOfVec::from_iter([AttributeTypeAndValue {
             oid: CN,
             value: Utf8StringRef::new(common_name)?.into(),
         }])?),
-        RelativeDistinguishedName(SetOfVec::from_iter([AttributeTypeAndValue {
+        RelativeDistinguishedName::from(SetOfVec::from_iter([AttributeTypeAndValue {
             oid: SERIAL_NUMBER,
             value: (&PrintableString::new(serial_number)?).into(),
         }])?),
